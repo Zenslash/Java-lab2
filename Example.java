cbd6219a762e3332e4a26022773f10e04172556c
Artem <34547317+Zenslash@users.noreply.github.com>
2021-09-30 10:31:38 +0300

    Update Example.java

--- Example.java
+++ Example.java
@@ -5,10 +5,10 @@ public class Example
 
 public static void main(String[] args) 
 	{
-    	Transport tr1 = new Automobil("марка1", "модель1",44.7, 5, 90.5, null);
-    	Transport tr2 = new Bus("марка2", "модель2", 66.3, 3, 40);
-    	Transport tr3 = new Trailer("марка3", "модель3",66.3, 3, 40, 34.5, 34.5);
-    	//Transport tr4 = new Automobil("марка1", "модель1",44.7, 5, 90.5, trailer);
+    	Automobil tr1 = new Automobil("Г¬Г Г°ГЄГ 1", "Г¬Г®Г¤ГҐГ«Гј1",44.7, 5, 90.5, null);
+    	Bus tr2 = new Bus("Г¬Г Г°ГЄГ 2", "Г¬Г®Г¤ГҐГ«Гј2", 66.3, 3, 40);
+    	Trailer tr3 = new Trailer("Г¬Г Г°ГЄГ 3", "Г¬Г®Г¤ГҐГ«Гј3",66.3, 3, 40, 34.5, 34.5);
+    	//Automobil tr4 = new Automobil("Г¬Г Г°ГЄГ 1", "Г¬Г®Г¤ГҐГ«Гј1",44.7, 5, 90.5, trailer);
     
     tr1.Info();
     tr2.Info();
